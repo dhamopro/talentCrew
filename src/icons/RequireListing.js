bda4e673453efb90d26c7b7234c356992773ea00
t.dhamodharan <t.dhamodharan@gmail.com>
2024-08-11 15:43:24 +0530

    icons

---
+++ src/icons/RequireListing.js
@@ -0,0 +1,8 @@
+const RequirementIcon = () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4 6H20V8H4V6ZM4 10H20V12H4V10ZM4 14H20V16H4V14ZM7 18L10 21L17 14" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+  
+  export default RequirementIcon;
+  
